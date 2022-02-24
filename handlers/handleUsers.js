06cd8bea7d176f4a3ad130c0d8133eb2d54ca322
AnnupKapur <AnnupKapur@gmail.com>
2022-02-24 00:58:45 +0000

    add user

---
+++ handlers/handleUsers.js
@@ -0,0 +1,22 @@
+const userRecord = require("../models/modelUserExercise.js");
+
+// ------------------------
+// POST : NEW USER
+// ------------------------
+const post_newUser = (req, res) => {
+	if(req.body.username.length===0){
+		return res.json({"error" : "You must enter a username"})
+	} else {
+		let newUser = new userRecord({
+			"username": req.body.username
+		})
+		newUser.save((err, data)=>{
+			if(err){return console.log(`Error: ${err}`)}
+			return res.json(data);
+		});
+	}
+}
+
+const get_userDetails = ()
+
+exports.post_newUser = post_newUser;
--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+var bodyParser = require("body-parser");
+const mongo = require("mongodb");
+const mongoose = require('mongoose');
+const handler = require('./handlers/handleUsers.js');
+
+mongoose.connect(process.env.MONGO_URI, {useMongoClient: true}, (err)=>{
+  if(err) {return console.log(`Error: ${err}`)};
+  console.log(`MongoDB Connection is: ${mongoose.connection.readyState}` );
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,10 +20,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
+
+// POST : CREATE USER : RETURN USER ID : api/users/:USERID
+app.post("/api/users", handler.post_newUser);
+
+// GET : RETURN ARRAY LIST ALL USERS : api/users/
+  // { USERNAME: USERNAME, USERID: ID}
+
+// POST : ADD EXERCISE : api/users/:_id/exercises
+  // PAYLOAD : description, duration ?date (if null currentDate)
+  // POST RETURN : { EXERCISE DATA }
+
+// GET : RETURN EXERCISE OF USER : api/users/:_id/logs
+
+// GET : RETURN EXERCISES OF USER : api/users/:_id/logs
+  // OPTIONAL PARAMS: [FROM, TO, LIMIT]
